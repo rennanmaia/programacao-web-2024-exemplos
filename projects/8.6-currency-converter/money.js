86e3d9d207202eaab4a1cf5e2f6fb3ca0e6ec3c6
Rennan Maia <rennanmaia@gmail.com>
2024-07-31 21:06:20 -0300

    feat: Conversion function completed
    The page capture the value to convert, get the currency list from
    API, after get the rates from API to and then make the conversion.

--- projects/8.6-currency-converter/money.js
+++ projects/8.6-currency-converter/money.js
@@ -24,7 +24,7 @@ async function getCurrencyRate(from) {
     let apiKey = "bd6c24ec1b-b8ea5ef81b-sh5h9z";
     let route = "/fetch-all/";
     let endpoint = "https://api.fastforex.io";
-    let url = endpoint + route + "?api_key=" + apiKey + "&base=" + from ;
+    let url = endpoint + route + "?api_key=" + apiKey + "&from=" + from ;
     console.log(url);
     const options = {
         method: 'GET',
@@ -39,13 +39,11 @@ async function getCurrencyRate(from) {
 
 }
 
-async function convert(amount, from, to) {
-    return amount * (to / from);
-}
-
 async function loadCurrencies() {
     let currencies = await getCurrencies();
+    console.log("Currencies: " , currencies);
     let currencyList = Object.entries(currencies);
+    console.log(" Currency List: " , currencyList);
 
     let selectFrom = document.getElementById("currencyFrom");
     let selectTo = document.getElementById("currencyTo");
@@ -69,25 +67,47 @@ async function loadCurrencies() {
 
 async function loadCurrencyRates() {
     let from = document.getElementById("currencyFrom").value;
-    console.log(from);
+    console.log("from: " , from);
 
     let currencyRates = await getCurrencyRate(from);
-    console.log(currencyRates);
+    console.log("Currency rates: " , currencyRates);
+    let currencyRateList = Object.entries(currencyRates);
+    console.log(" Currency Rate List: " , currencyRateList);
 
     let selectTo = document.getElementById("currencyTo");
-    selectTo.innerHTML = "";
 
-    for (let i = 0; i < currencyRates.length; i++) {
-        let optionFrom = document.createElement("option");
-        optionFrom.value = currencyList[i][0];
-        optionFrom.text = currencyList[i][0] + " - " + currencyList[i][1];
+    for (let i = 0; i < selectTo.options.length; i++) {
+        let option = selectTo.options[i];
+        let currentText = option.text;
+        let currentTextArray = currentText.split(" - ");
+        let currentCurrencyName = currentTextArray[1];
+        let currentCurrencyAbbreviation = currentTextArray[0];
+
+        let index = currencyRateList.findIndex((element) => element[0] === currentCurrencyAbbreviation);
         
-        let optionTo = document.createElement("option");
-        optionTo.value = currencyList[i][0];
-        optionTo.text = currencyList[i][0] + " - " + currencyList[i][1];
+        let newText = currentCurrencyAbbreviation + " - " + currentCurrencyName + " - " + currencyRateList[index][1];
 
-        selectFrom.add(optionFrom);
-        selectTo.add(optionTo);
+        option.text = newText;
+        option.value = currencyRateList[index][1];
+    }
+
+}
+
+function convert(amount, rate) {
+    return amount * rate;
+}
+
+function convertCurrency() {
+
+    let amount = document.getElementById("amount").value;
+    let rate = document.getElementById("currencyTo").value;
+    if (!(isNaN(parseFloat(rate)))) {
+        let convertedAmount = convert(amount, rate);
+        document.getElementById("convertedAmount").value = convertedAmount;
+        document.getElementById("message").hidden = true;
+    } else {
+        document.getElementById("message").innerHTML = "Por favor, capture as taxas de conversão através dos botões Listar moedas e Pegar taxas";
+        document.getElementById("message").hidden = false;
     }
 
 }
